7e27a0bab147c4c32ac3a6957bc66184f3534f19
metastable-void <git@yuka.h09.eu>
2025-09-30 14:55:47 +0900

    fix

--- src/ipv6.rs
+++ src/ipv6.rs
@@ -84,7 +84,8 @@ impl Dhcp6Client {
         let mut buf = Vec::with_capacity(1500);
         let mut e = Encoder::new(&mut buf);
         msg.encode(&mut e).map_err(|_| std::io::Error::new(ErrorKind::InvalidData, "DHCPv6 encoding failed"))?;
-        let sentlen = self.socket.send_to(&buf, ("ff02::1:2", Self::SERVER_PORT))?;
+        let buf = e.buffer_filled();
+        let sentlen = self.socket.send_to(buf, ("ff02::1:2", Self::SERVER_PORT))?;
         if sentlen < buf.len() {
             log::error!("Packet ({} Bytes) not sent in whole", buf.len());
         } else {
